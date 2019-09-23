3190cfa314254cd7395791b87c93ed03ce20dc25
stp4 <w.peter@statistik-peter.at>
2019-09-23 07:08:37 +0200

    Funktionsname auf pagebreak() geändert

--- R/add_new_page.R
+++ R/add_new_page.R
@@ -1,27 +1,23 @@
-#' add_new_page
+#' pagebreak
 #'
 #' Neue Seite bei copy and paste in Word.
 #'
 #' @return nichts
 #' @export
 #' 
-add_new_page<-
-  function() {
-    if (which_output() == "html")
-      HTML_default('<br style="page-break-before: always">')
-    else if(which_output() == "markdown") cat('\\pagebreak')
-    else cat("\n----------------\n\n")
-  }
+#' @examples 
+#' # in MD-Files
+#' # `r pagebreak()`
+#' 
+ 
+pagebreak <- function() {
+  if(knitr::is_latex_output())
+    return("\\newpage")
+  else if(knitr::is_html_output())
+    return('<div style="page-break-before: always;" />')
+  else  if (which_output() == "html")
+      HTML_default('<br style="page-break-before: always">') 
+  else cat("\n\n***\n\n")
+}
 
-
-# add_new_page <- function() {
-#   if (which_output() == "html")
-#     code <- '<br style="page-break-before: always">'
-#   
-#   else
-#     code <- "\n----------------\n\n"
-#   HTML_default(code)
-#   
-# }
-
-
+#pagebreak()
